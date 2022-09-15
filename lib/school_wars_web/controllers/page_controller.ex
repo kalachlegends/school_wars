62698c07d2040e9b9309846562282e4349170d4a
artem <artem.kalachev.2002@gmail.com>
2022-09-15 17:48:37 +0600

    landing page

--- lib/school_wars_web/controllers/page_controller.ex
+++ lib/school_wars_web/controllers/page_controller.ex
@@ -2,18 +2,12 @@ defmodule SchoolWarsWeb.PageController do
   use SchoolWarsWeb, :controller
 
   def index(conn, _params) do
-    {:ok, token} = User.Services.login_user("lol3", "haha")
-    put_session(conn, :token, token)
-    |> render("index.html")
+    render(conn, "index.html")
   end
 
-  def index_token(conn, _params) do
-    conn
-    |> put_flash(:info, Session.read(get_session(conn, :token)).data.account.login)
-    |> render("index.html", data: Session.read(get_session(conn, :token)).data)
-  end
-
-
-
-
+  # def index_token(conn, _params) do
+  #   conn
+  #   |> put_flash(:info, Session.read(get_session(conn, :token)).data.account.login)
+  #   |> render("index.html", data: Session.read(get_session(conn, :token)).data)
+  # end
 end

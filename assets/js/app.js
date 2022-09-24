cf8da404aa66fa49bef434907eef795e7f291a8e
chudickgumanoid <sashaq0708@gmail.com>
2022-09-24 17:29:35 +0600

    uikit add

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,6 @@
 // We import the CSS which is extracted to its own file by esbuild.
 
-
-import '../scss/style.scss'
-
+import "../scss/style.scss";
 
 // If you want to use Phoenix channels, run `mix help phx.gen.channel`
 // to get started and then uncomment the line below.
@@ -22,60 +20,134 @@ import '../scss/style.scss'
 //
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html"
+import "phoenix_html";
 // Establish Phoenix Socket and LiveView configuration.
-import { Socket } from "phoenix"
-import { LiveSocket } from "phoenix_live_view"
-import topbar from "../vendor/topbar"
+import { Socket } from "phoenix";
+import { LiveSocket } from "phoenix_live_view";
+import topbar from "../vendor/topbar";
 
-
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken } })
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: { _csrf_token: csrfToken },
+});
 
 // Show progress bar on live navigation and form submits
-topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
-window.addEventListener("phx:page-loading-start", info => topbar.show())
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
+window.addEventListener("phx:page-loading-start", (info) => topbar.show());
+window.addEventListener("phx:page-loading-stop", (info) => topbar.hide());
 
 // connect if there are any LiveViews on the page
-liveSocket.connect()
+liveSocket.connect();
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
+window.liveSocket = liveSocket;
 import $ from "jquery";
 
-import Swal from 'sweetalert2/dist/sweetalert2.js'
+import Swal from "sweetalert2/dist/sweetalert2.js";
 globalThis.$ = $;
 globalThis.Swal = Swal;
 
-// DROP DOWN 
+// DROP DOWN
 
 $(document).ready(function () {
+  $(".header_burger ").click(function (event) {
+    $(".header_burger,.nav,.header").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
 
-    $('.header_burger ').click(function (event) {
-        $('.header_burger,.nav,.header').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
+  $(".drop-down").click(function (event) {
+    let title = this.querySelector(".title-dropdown"),
+      submenu = this.querySelector(".drop-down__submenu"),
+      originalEvent = event.originalEvent,
+      text;
 
+    if (originalEvent.path[1].className == "drop-down__item")
+      text = originalEvent.path[1].textContent;
+    if (originalEvent.path[2].className == "drop-down__item")
+      text = originalEvent.path[2].textContent;
 
-
-    $(".drop-down").click(function (event) {
-        let title = this.querySelector(".title-dropdown"),
-            submenu = this.querySelector(".drop-down__submenu"),
-            originalEvent = event.originalEvent,
-            text
-
-        if (originalEvent.path[1].className == "drop-down__item") text = originalEvent.path[1].textContent
-        if (originalEvent.path[2].className == "drop-down__item") text = originalEvent.path[2].textContent
-
-        if (originalEvent.path[1].className == "drop-down__item" ||
-            originalEvent.path[2].className == "drop-down__item") {
-            title.textContent = text
-        }
-        submenu.classList.toggle("active")
-
-    })
+    if (
+      originalEvent.path[1].className == "drop-down__item" ||
+      originalEvent.path[2].className == "drop-down__item"
+    ) {
+      title.textContent = text;
+    }
+    submenu.classList.toggle("active");
+  });
 });
+
+var x, i, j, l, ll, selElmnt, a, b, c;
+x = document.getElementsByClassName("select-class");
+l = x.length;
+for (i = 0; i < l; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  ll = selElmnt.length;
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < ll; j++) {
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function (e) {
+      var y, i, k, s, h, sl, yl;
+      s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+      sl = s.length;
+      h = this.parentNode.previousSibling;
+      for (i = 0; i < sl; i++) {
+        if (s.options[i].innerHTML == this.innerHTML) {
+          s.selectedIndex = i;
+          h.innerHTML = this.innerHTML;
+          y = this.parentNode.getElementsByClassName("same-as-selected");
+          yl = y.length;
+          for (k = 0; k < yl; k++) {
+            y[k].removeAttribute("class");
+          }
+          this.setAttribute("class", "same-as-selected");
+          break;
+        }
+      }
+      h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function (e) {
+    e.stopPropagation();
+    closeAllSelect(this);
+    this.nextSibling.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+  });
+}
+function closeAllSelect(elmnt) {
+  var x,
+    y,
+    i,
+    xl,
+    yl,
+    arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  xl = x.length;
+  yl = y.length;
+  for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i);
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+document.addEventListener("click", closeAllSelect);
